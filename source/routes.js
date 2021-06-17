23a13a552bdb636353399d944a799a778da1bd63
krisnakris <krisna.post@gmail.com>
2021-06-17 20:18:36 +0700

    initial commit

---
+++ source/routes.js
@@ -0,0 +1,33 @@
+const {
+  deleteNoteByIdHandler, addNoteHandler, getAllNotes, getNoteByIdHandler, editNoteByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotes,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
